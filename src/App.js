584cd0047f781521368e3fabf0b623e3f7e3e84c
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-10-16 17:07:39 -0400

    feat: dynamically render Snackbar in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import MessageInput from "./components/MessageInput";
+import Snackbar from "./components/Snackbar";
+import SnackbarContext from "./components/store/snackbar-context";
 
-const App = () => <MessageInput />;
+const App = () => {
+  const snackbarCtx = useContext(SnackbarContext);
+  return (
+    <>
+      <MessageInput />
+      {snackbarCtx.isDisplayed && <Snackbar />}
+    </>
+  );
+};
 
 export default App;
