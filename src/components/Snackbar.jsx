e06aed2a6932e9f0fdc61d33e3b9425da490f8b8
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-10-16 17:14:13 -0400

    feat: render context message state in snackbar

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./Snackbar.css";
+import SnackbarContext from "./store/snackbar-context";
 
 const Snackbar = () => {
+  const snackbarCtx = useContext(SnackbarContext);
   return (
     <div className="snackbar__container">
-      <div className="snackbar__label">Hello!</div>
+      <div className="snackbar__label">{snackbarCtx.msg}</div>
       <div className="snackbar__dismiss">&times;</div>
     </div>
   );
