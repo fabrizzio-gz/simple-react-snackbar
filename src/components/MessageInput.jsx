b4543acafd851a2f131549f28a4ec7057e76d0c4
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-10-16 17:24:30 -0400

    feat: trigger snackbar from message input

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 
 import "./MessageInput.css";
+import SnackbarContext from "./store/snackbar-context";
 
 const MessageInput = () => {
+  const inputRef = useRef();
+  const snackbarCtx = useContext(SnackbarContext);
+
+  const clickHandler = () => {
+    const msg = inputRef.current.value;
+    snackbarCtx.displayMsg(msg);
+  };
+
   return (
     <div className="app__container">
       <div className="app__center">
         <label htmlFor="snackbar-msg">Message</label>
-        <input id="snackbar-msg" type="text" />
+        <input ref={inputRef} id="snackbar-msg" type="text" />
       </div>
       <div className="app__center">
-        <button className="app__button">Show snackbar</button>
+        <button className="app__button" onClick={clickHandler}>
+          Show snackbar
+        </button>
       </div>
     </div>
   );
