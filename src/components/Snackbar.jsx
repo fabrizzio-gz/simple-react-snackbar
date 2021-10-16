61d9528a1a97882b39b544c2269254d7876d36a2
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-10-16 17:19:33 -0400

    feat: add handler to dismiss button on snackbar

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -5,10 +5,13 @@ import SnackbarContext from "./store/snackbar-context";
 
 const Snackbar = () => {
   const snackbarCtx = useContext(SnackbarContext);
+
   return (
     <div className="snackbar__container">
       <div className="snackbar__label">{snackbarCtx.msg}</div>
-      <div className="snackbar__dismiss">&times;</div>
+      <div className="snackbar__dismiss" onClick={snackbarCtx.onClose}>
+        &times;
+      </div>
     </div>
   );
 };
